0205391e8c40cb65ad9fcff5fec6b1a4816d4087
Paulo Sérgio <paulomg1996@gmail.com>
2026-02-03 16:16:44 -0300

    Revert "Refactor proxy.js to use environment variable for authorization token"
    This reverts commit bf2187b5b48430e816ff2bce984b0f97a20f93d8.

--- proxy.js
+++ proxy.js
@@ -2,17 +2,17 @@ const express = require("express")
 const axios = require("axios")
 const cors = require("cors")
 const app = express()
-const TOKEN = process.env.TOKEN
 
 app.use(cors()) // Libera o acesso para o seu frontend
 app.use(express.json())
 
 app.get("/get-odds", async (req, res) => {
+	const token = req.headers.authorization
 
 	try {
 		const response = await axios.get("https://filtrodeapostas.com/api/odds", {
 			headers: {
-				authorization: TOKEN,
+				authorization: token,
 				Referer: "https://filtrodeapostas.com/odds",
 				"User-Agent":
 					"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0",
@@ -27,11 +27,12 @@ app.get("/get-odds", async (req, res) => {
 // No seu proxy.js
 app.get("/get-details", async (req, res) => {
 	const slug = req.query.slug // Pega o identificador enviado pelo frontend
+	const token = req.headers.authorization
 
 	try {
 		const response = await axios.get(`https://filtrodeapostas.com/api/odds/${slug}`, {
 			headers: {
-				authorization: TOKEN,
+				authorization: token,
 				Referer: "https://filtrodeapostas.com/odds",
 				"User-Agent": "Mozilla/5.0...",
 			},
